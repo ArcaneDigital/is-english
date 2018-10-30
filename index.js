190841b7bc9e42c0c2a13ace81e60bb7b6cc8fb3
Jay Goodfellow <j.goodfellow@arcane.ws>
2018-10-30 14:04:18 -0400

    Added rough check for Indonesian

--- index.js
+++ index.js
@@ -2,13 +2,33 @@ module.exports = (text, options = {}) => {
   if (!text) throw new Error("Text is required");
   let sample = text.replace(/\s+/g, "").replace(/(\s?-\s)/g, "");
 
-  const found = sample.match(/(?=[^\u0000-\u007F]+)(?=[^’“”–©])/gim);
+  const found = sample.match(/(?=[^\u0000-\u007F]+)(?=[^’“”–©])/gim) || [];
 
-  if (!found) return true;
+  const sup = indocheck(sample);
 
-  const percent = found.length / sample.length;
+  const percent = (found.length + sup) / sample.length;
 
   if (percent < 0.001) return true;
 
   return false;
 };
+
+function indocheck(sample) {
+  const words = [
+    "selamatpagi",
+    "selamatsiang",
+    "selamatmalam",
+    "beberapa",
+    "bersama",
+    "dilakukan",
+    "terimakasih",
+    "permisimas",
+    "permisiadek",
+    "permisipak",
+    "berbagi"
+  ];
+  return words.reduce((count, word) => {
+    count += (sample.match(new RegExp(word, "gim")) || []).length;
+    return count;
+  }, 0);
+}
